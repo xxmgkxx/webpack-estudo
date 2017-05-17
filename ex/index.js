235108d7325af4d5e0e8704387b6b13f4239b96e
Aluan <aluan.cabral@outlook.com>
2017-05-16 22:35:33 -0300

    Interpretando o Operador Spread (Plugin Babel)

--- ex/index.js
+++ ex/index.js
@@ -1,4 +1,14 @@
-import Pessoa from './pessoa';
+const produto = {
+    nome: 'Caneta Bic Preta',
+    preco: 1.90,
+    desconto: 0.05
+}
 
-const pessoa = new Pessoa('Aluan');
-console.log(pessoa.toString());
+function clone(objeto) {
+    return { ...objeto }
+}
+
+const novoProduto = clone(produto);
+novoProduto.nome = 'Caneta Bic Azul';
+
+console.log(produto, novoProduto);
